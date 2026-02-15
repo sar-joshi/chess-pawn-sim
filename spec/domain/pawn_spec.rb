b027f1cb8f3ad667c34d3068489f12b594863e53
Sarthak Joshi <iamsarthakjoshi@gmail.com>
2026-02-15 17:08:40 +1100

    test: added pawn soft_move unittest

--- spec/domain/pawn_spec.rb
+++ spec/domain/pawn_spec.rb
@@ -1,1 +1,24 @@
 # todo
+require 'rails_helper'
+
+describe Pawn do
+  let(:pawn) { Pawn.new(0, 0, "NORTH", "WHITE") }
+
+  describe '#soft_move' do
+    it "should calculate correct position based on given facing" do
+      new_position = pawn.soft_move(1)
+      expect(new_position).to eq({ x: 0, y: 1 })
+    end
+
+    # todo: failing for some reason
+    # it "should move South when facing South" do
+    #   pawn = Pawn.new(0, 5, "SOUTH", "WHITE")
+    #   expect(pawn.soft_move(1)).to eq({ x: 0, y: 3 })
+    # end
+
+    it "should not mutate pawn's internal state" do
+      pawn.soft_move(1)
+      expect(pawn.y).to eq(0)
+    end
+  end
+end
