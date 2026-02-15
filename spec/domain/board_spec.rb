7bbb79c8aaeae83c3b16e0c24f0c8f04e081e76c
Sarthak Joshi <iamsarthakjoshi@gmail.com>
2026-02-15 17:00:58 +1100

    feat: added board unit tests

--- spec/domain/board_spec.rb
+++ spec/domain/board_spec.rb
@@ -1,1 +1,18 @@
-# todo
+require "rails_helper"
+
+describe Board do
+  let(:board) { Board.new }
+
+  describe "#position_in_bound?" do
+    it "should return true for valid coordinates inbound" do
+      expect(board.position_in_bound?(0, 0)).to be true
+      expect(board.position_in_bound?(7, 7)).to be true
+      expect(board.position_in_bound?(4, 2)).to be true
+    end
+
+    it "should return false for coordinates out of bound" do
+      expect(board.position_in_bound?(-1, -1)).to be false
+      expect(board.position_in_bound?(8, 8)).to be false
+    end
+  end
+end
